e322f020b36f7d003d8fa52f955ce94fc572883a
LeonardoYur <162757807+LeonardoYur@users.noreply.github.com>
2025-09-12 19:16:22 -0300

    docker-entrypoint.sh

--- aula05/docker-entrypoint.sh
+++ aula05/docker-entrypoint.sh
@@ -11,10 +11,10 @@ done
 echo "MySQL iniciado."
 
 # Define a senha para o usuário 'root'
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456'; FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'leo'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456'; FLUSH PRIVILEGES;"
 
 # Verifica se o banco de dados 'britinho' já existe.
-if ! mysql -u root -p'minhasenha123' -e "USE britinho;" 2>/dev/null; then
+if ! mysql -u root -p'123456' -e "USE dbaula05" 2>/dev/null; then
     echo "Banco de dados 'dbaula05' não encontrado. Executando script SQL..."
     # Adicione a flag --default-character-set=utf8mb4 aqui
     mysql -u root -p'123456' --default-character-set=utf8mb4 < /docker-entrypoint-initdb.d/banco_de_dados.sql
