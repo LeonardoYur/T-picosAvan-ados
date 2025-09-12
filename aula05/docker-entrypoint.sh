7fb0b2b096e4b1ce235cc645dc5a28ee10390a20
LeonardoYur <162757807+LeonardoYur@users.noreply.github.com>
2025-09-12 19:14:40 -0300

    docker-entrypoint.sh

--- aula05/docker-entrypoint.sh
+++ aula05/docker-entrypoint.sh
@@ -11,15 +11,15 @@ done
 echo "MySQL iniciado."
 
 # Define a senha para o usuário 'root'
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'minhasenha123'; FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456'; FLUSH PRIVILEGES;"
 
 # Verifica se o banco de dados 'britinho' já existe.
 if ! mysql -u root -p'minhasenha123' -e "USE britinho;" 2>/dev/null; then
-    echo "Banco de dados 'britinho' não encontrado. Executando script SQL..."
+    echo "Banco de dados 'dbaula05' não encontrado. Executando script SQL..."
     # Adicione a flag --default-character-set=utf8mb4 aqui
-    mysql -u root -p'minhasenha123' --default-character-set=utf8mb4 < /docker-entrypoint-initdb.d/banco_de_dados.sql
+    mysql -u root -p'123456' --default-character-set=utf8mb4 < /docker-entrypoint-initdb.d/banco_de_dados.sql
 else
-    echo "Banco de dados 'britinho' já existe. Pulando a execução do script SQL."
+    echo "Banco de dados 'dbaula05' já existe. Pulando a execução do script SQL."
 fi
 
 # Inicia o Apache em primeiro plano, mantendo o contêiner ativo
